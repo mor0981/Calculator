aa86f58aca9d29b680770df2f9434d161d3f4d6a
Mor Biton <34809486+mor0981@users.noreply.github.com>
2022-11-06 22:08:33 +0200

    Producs

--- Calculator/Producs.cs
+++ Calculator/Producs.cs
@@ -7,12 +7,18 @@ namespace Calculator
     {
         [BsonElement("Trade_Item_Description")]
         public string Trade_Item_Description { get; set; }
-        public string Fats { get; set; }
-        public string Carbohydrates { get; set; }
-        public string Proteins { get; set; }
-        public string DietaryFiber { get; set; }
-        public string SaturatedFattyAcids { get; set; }
-        public string Dessert { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Fats { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Carbohydrates { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Proteins { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float DietaryFiber { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float SaturatedFattyAcids { get; set; }
+        [BsonRepresentation(BsonType.Int32, AllowTruncation = true)]
+        public float Dessert { get; set; }
 
         public ObjectId _id { get; set; }
     }
